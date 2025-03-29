d9d3f51cd5c4bc5812015ac1b185f5bbb51c3304
Jonty <89488176+Jomlom@users.noreply.github.com>
2025-03-29 17:53:30 +0000

    Return items correctly on screen closes
    - injects at head of onclose method to return items to origin instead of player inventory (this was done by a different function to when you change recipes etc)
    - toggleable by a new boolean in my interface (will lead into issue #3 functionality)

---
+++ src/main/java/com/jomlom/recipebookaccess/mixin/ScreenHandlerMixin.java
@@ -0,0 +1,39 @@
+package com.jomlom.recipebookaccess.mixin;
+
+import com.jomlom.recipebookaccess.api.RecipeBookInventoryProvider;
+import com.jomlom.recipebookaccess.util.RecipeBookAccessUtils;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.screen.AbstractCraftingScreenHandler;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.slot.Slot;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ScreenHandler.class)
+public abstract class ScreenHandlerMixin {
+
+    @Inject(
+            method = "onClosed(Lnet/minecraft/entity/player/PlayerEntity;)V",
+            at = @At("HEAD")
+    )
+    private void onClosedInject(PlayerEntity player, CallbackInfo ci) {
+        if (!(player instanceof ServerPlayerEntity)) return;
+        ScreenHandler handler = (ScreenHandler) (Object) this;
+        if (handler instanceof AbstractCraftingScreenHandler screenHandler && screenHandler instanceof RecipeBookInventoryProvider customPop) {
+            if (!customPop.persistentInventory()) {
+                for (Slot slot : screenHandler.getInputSlots()) {
+                    ItemStack stack = slot.getStack().copy();
+                    boolean returned = RecipeBookAccessUtils.tryReturnItemToOrigin(slot, stack);
+                    if (!returned) {
+                        player.getInventory().offer(stack, false);
+                    }
+                    slot.setStackNoCallbacks(stack);
+                }
+            }
+        }
+    }
+}
