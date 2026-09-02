228cb93c2fa56845f016d2829063e45ca53725a0
Jonty <89488176+Jomlom@users.noreply.github.com>
2026-09-02 14:38:08 +0100

    bug/performance fixes

--- common/src/client/java/com/jomlom/recipebookaccess/network/ClientItemsReciever.java
+++ common/src/client/java/com/jomlom/recipebookaccess/network/ClientItemsReciever.java
@@ -9,17 +9,25 @@ public class ClientItemsReciever {
 
     private static List<ItemStack> itemStacks = new ArrayList<>();
     private static Runnable onUpdate = null;
+    private static boolean requestPending = false;
+
+    public static boolean isRequestPending() {
+        return requestPending;
+    }
 
     public static void setOnUpdate(Runnable callback) {
         onUpdate = callback;
+        requestPending = true;
     }
 
     public static void recieveItems(Minecraft client, List<ItemStack> items) {
         client.execute(() -> {
             itemStacks = new ArrayList<>(items);
+            requestPending = false;
             if (onUpdate != null) {
-                onUpdate.run();
+                Runnable callback = onUpdate;
                 onUpdate = null;
+                callback.run();
             }
         });
     }
